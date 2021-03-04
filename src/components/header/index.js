d3b4db8aef9dcfb48cab24568e14962c90ec0568
João Pedro Barreto <joao.fb@icloud.com>
2021-03-03 23:52:47 -0300

    Adding logo to header and separating brand from the rest of the header.

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -2,17 +2,19 @@ import React from "react";
 
 import { Navbar, Col } from "react-bootstrap";
 
-import styles from "./header.module.css"
+import styles from "./header.module.css";
 
 const Header = () => {
   return (
-    <Navbar
-      expand="lg"
-      className={styles.header}
-    >
-      <Col xs={2} className="pl-0">
+    <Navbar expand="lg" className={styles.header}>
+      <Col xs={2} className="px-3 py-2" style={{ backgroundColor: "#151542" }}>
         <Navbar.Brand href="#home" style={{ color: "white", fontWeight: 600 }}>
-          Drizzle Tech
+          <img
+            src="/images/drizzle.png"
+            alt="Logo"
+            style={{ width: "1.4rem", marginRight: "0.5rem" }}
+          />
+          Drizzle
         </Navbar.Brand>
       </Col>
     </Navbar>
